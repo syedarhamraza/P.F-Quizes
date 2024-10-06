f6f9b5a267c4f852f517d4141efe482cdce69c92
arhammastery <arhamraza399@gmail.com>
2024-10-06 18:08:19 +0500

    added pf quiz 1

---
+++ pf-quiz-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1 = 10;
+    float num2 = 20;
+    float sum = 0;
+    float avg = 0;
+
+    sum = num1 + num2;
+    avg = (num1 + num2) / 2;
+
+    return 0;
+}
